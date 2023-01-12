403535435840f7396dce4e40efeb532c84ece51f
hkh0105 <hkh9601@naver.com>
2023-01-12 18:37:25 +0900

    refactor : recoil todoState의 initialState 변수로 분리

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,20 +1,22 @@
 import { atom } from 'recoil';
 import { TodoListType } from '@src/types';
 
+export const initailTodo = [
+  {
+    id: 1,
+    title: 'todo 1',
+    content: 'Initial Content by Han',
+    status: 'todo',
+  },
+  {
+    id: 2,
+    title: 'todo 2',
+    content: 'Second Content by Han',
+    status: 'todo',
+  },
+];
+
 export const todoState = atom<TodoListType>({
   key: 'todoList',
-  default: [
-    {
-      id: 1,
-      title: 'todo 1',
-      content: 'Initial Content by Han',
-      status: 'todo',
-    },
-    {
-      id: 2,
-      title: 'todo 2',
-      content: 'Second Content by Han',
-      status: 'todo',
-    },
-  ],
+  default: initailTodo,
 });
