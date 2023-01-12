fdc5858cfc0a7a36f5119adcb82e71147d97ce60
hkh0105 <hkh9601@naver.com>
2023-01-12 17:03:40 +0900

    feat : Type 선언

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,26 @@
+import { ChangeEvent, MouseEvent } from 'react';
+import { RecoilState } from 'recoil';
+
 export interface ButtonProps {
   label: string;
+  onClick?: () => void;
 }
 
 export interface InputProps {
   placeholder: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface Todo {
+  id: number;
+  title: string;
+  status: string;
+  content?: string;
+}
+
+export type TodoListType = Todo[];
+
+export interface TodoFormRecoilObserverProps {
+  node: RecoilState<TodoListType>;
+  onChange: (value: TodoListType) => void;
 }
