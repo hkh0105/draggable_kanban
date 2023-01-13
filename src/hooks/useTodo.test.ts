6d1ea38e2c7f77599ea53c75e995571d483036d5
hkh0105 <hkh9601@naver.com>
2023-01-13 23:40:45 +0900

    test : patchTodo test 코드 구현

--- src/hooks/useTodo.test.ts
+++ src/hooks/useTodo.test.ts
@@ -3,8 +3,18 @@ import { RecoilRoot } from 'recoil';
 
 import { initailTodo } from '@src/recoil';
 import useTodo from './useTodo';
+import { TodoListType } from '@src/types';
+import { v4 as uuidv4 } from 'uuid';
+
+jest.mock('uuid');
 
 describe('useTodo Test', () => {
+  beforeAll(() => {
+    (uuidv4 as jest.Mock).mockImplementation(() => {
+      return '1';
+    });
+  });
+
   test('addTodo function unit test', () => {
     const { result } = renderHook(() => useTodo(), {
       wrapper: RecoilRoot,
@@ -18,14 +28,30 @@ describe('useTodo Test', () => {
       addTodo('Test');
     });
 
-    const todoList = [
+    const todoList: TodoListType = [
       ...initailTodo,
       {
-        id: 3,
+        id: uuidv4(),
         status: 'todo',
         title: 'Test',
       },
     ];
     expect(result.current.todo).toEqual(todoList);
+  });
+  test('patchTodo function unit test', () => {
+    const { result } = renderHook(() => useTodo(), {
+      wrapper: RecoilRoot,
+    });
+
+    const { todo, patchTodo } = result.current;
+
+    expect(todo).toEqual(initailTodo);
+
+    const id = todo[0].id;
+    act(() => {
+      patchTodo('status', 'inProgress', id);
+    });
+
+    expect(result.current.todo[0].status).toEqual('inProgress');
   });
 });
