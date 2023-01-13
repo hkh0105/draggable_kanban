3156b7a0913f9e9f31c199a1e922242f777e36b9
hkh0105 <hkh9601@naver.com>
2023-01-13 23:23:46 +0900

    test : TodoList 새로운 test 코드 구현

--- src/components/todoList/todoList.test.tsx
+++ src/components/todoList/todoList.test.tsx
@@ -1,8 +1,8 @@
-import { todoState } from '@src/recoil';
 import { TodoListType } from '@src/types';
 import { render, screen } from '@testing-library/react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
+import { todoState, normalTodoState } from '@src/recoil';
 
 import TodoList from './TodoList';
 
@@ -12,12 +12,24 @@ jest.mock('uuid');
 describe('TodoList test', () => {
   beforeAll(() => {
     (useRecoilValue as jest.Mock).mockImplementation((selector) => {
+      if (selector === normalTodoState) {
+        return [
+          { title: 'Todo 2' },
+          { title: 'Todo 4' },
+          { title: 'Todo 7' },
+        ] as TodoListType;
+      }
+    });
+    (useRecoilState as jest.Mock).mockImplementation((selector) => {
       if (selector === todoState) {
         return [
-          { title: 'Todo 1' },
-          { title: 'Todo 2' },
-          { title: 'Todo 3' },
-        ] as TodoListType;
+          [
+            { title: 'Todo 1' },
+            { title: 'Todo 2' },
+            { title: 'Todo 3' },
+          ] as TodoListType,
+          (value: string) => value,
+        ];
       }
     });
     (uuidv4 as jest.Mock).mockImplementation(() => {
@@ -28,9 +40,8 @@ describe('TodoList test', () => {
   test('check title', async () => {
     render(<TodoList />);
     const dragableBoxes = screen.getAllByTestId('dragable-box');
-
-    expect(dragableBoxes[0]).toHaveTextContent('Todo 1');
-    expect(dragableBoxes[1]).toHaveTextContent('Todo 2');
-    expect(dragableBoxes[2]).toHaveTextContent('Todo 3');
+    expect(dragableBoxes[0]).toHaveTextContent('Todo 2');
+    expect(dragableBoxes[1]).toHaveTextContent('Todo 4');
+    expect(dragableBoxes[2]).toHaveTextContent('Todo 7');
   });
 });
