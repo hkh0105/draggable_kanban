49462782b8d0ea07957737f3d4dd4f102b1a1367
hkh0105 <hkh9601@naver.com>
2023-01-13 23:24:03 +0900

    test : TodoForm test 코드 변경

--- src/components/todoform/todoform.test.tsx
+++ src/components/todoform/todoform.test.tsx
@@ -1,21 +1,27 @@
-import { todoState } from '../../recoil/index';
-import { TodoFormRecoilObserverProps, TodoListType } from '@src/types';
-import userEvent from '@testing-library/user-event';
-import { render, renderHook, screen } from '@testing-library/react';
-import { RecoilRoot, useRecoilValue } from 'recoil';
+import { render } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
+
+jest.mock('uuid');
 
 import TodoForm from './todoform';
+describe('TodoList test', () => {
+  beforeAll(() => {
+    (uuidv4 as jest.Mock).mockImplementation(() => {
+      return '1';
+    });
+  });
+  test('TodoForm  Test', async () => {
+    const { queryByPlaceholderText, getByText } = render(
+      <RecoilRoot>
+        <TodoForm />
+      </RecoilRoot>
+    );
 
-test('TodoForm  Test', async () => {
-  const { queryByPlaceholderText, getByText } = render(
-    <RecoilRoot>
-      <TodoForm />
-    </RecoilRoot>
-  );
-
-  expect(queryByPlaceholderText('todo 제목을 입력해주세요')).toHaveAttribute(
-    'placeholder',
-    'todo 제목을 입력해주세요'
-  );
-  expect(getByText('add')).toHaveTextContent('add');
+    expect(queryByPlaceholderText('todo 제목을 입력해주세요')).toHaveAttribute(
+      'placeholder',
+      'todo 제목을 입력해주세요'
+    );
+    expect(getByText('add')).toHaveTextContent('add');
+  });
 });
