290a3e0f27e1ac370f1b74c2117b9db2470b9f00
hkh0105 <hkh9601@naver.com>
2023-01-13 23:26:20 +0900

    feat : inProgressTodoState, normalTodoState, completedTodoState selector 구현

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,15 +1,16 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { TodoListType } from '@src/types';
+import { v4 as uuidv4 } from 'uuid';
 
 export const initailTodo = [
   {
-    id: 1,
+    id: uuidv4(),
     title: 'todo 1',
     content: 'Initial Content by Han',
     status: 'todo',
   },
   {
-    id: 2,
+    id: uuidv4(),
     title: 'todo 2',
     content: 'Second Content by Han',
     status: 'todo',
@@ -19,4 +20,55 @@ export const initailTodo = [
 export const todoState = atom<TodoListType>({
   key: 'todoList',
   default: initailTodo,
+});
+
+export const completedTodoState = selector<TodoListType>({
+  key: 'todoList/completed',
+  get: ({ get }) => {
+    const list = get(todoState);
+
+    return list.filter((item) => item.status === 'completed');
+  },
+});
+
+export const normalTodoState = selector<TodoListType>({
+  key: 'todoList/todo',
+  get: ({ get }) => {
+    const list = get(todoState);
+
+    return list.filter((item) => item.status === 'todo');
+  },
+});
+
+export const inProgressTodoState = selector<TodoListType>({
+  key: 'todoList/inProgress',
+  get: ({ get }) => {
+    const list = get(todoState);
+
+    return list.filter((item) => item.status === 'inProgress');
+  },
+});
+
+const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All',
+});
+
+export const filteredTodoListState = selector<TodoListType>({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoState);
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.status === 'completed');
+      case 'Show InProgress':
+        return list.filter((item) => item.status === 'inProgress');
+      case 'Show Todo':
+        return list.filter((item) => item.status === 'todo');
+      default:
+        return list;
+    }
+  },
 });
