34d8c6fe94a895f9204dbd50fb6c3ab17a223c79
hkh0105 <hkh9601@naver.com>
2023-01-14 18:07:18 +0900

    style : todoform layout 변경

--- src/components/todoform/todoform.tsx
+++ src/components/todoform/todoform.tsx
@@ -16,10 +16,14 @@ const TodoForm: FC = () => {
   };
 
   return (
-    <>
-      <Input placeholder='todo 제목을 입력해주세요' onChange={onChange} />
-      <Button label='add' onClick={clickAddHandler} />
-    </>
+    <div className='flex my-3 w-full justify-center'>
+      <Input
+        placeholder='todo 제목을 입력해주세요'
+        onChange={onChange}
+        className='border-solid border-2 border-slate-500 rounded-md w-1/3'
+      />
+      <Button label='add' onClick={clickAddHandler} className='mx-4' />
+    </div>
   );
 };
 
