2a5db650cedbb06eb696aac6c0cf90e392947a00
hkh0105 <hkh9601@naver.com>
2023-01-14 19:23:56 +0900

    test : layout test 코드 구현

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -8,10 +8,10 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
     <>
       <div className='flex w-screnn h-20  bg-stone-50'>
         <Link href='/'>
-          <AiOutlineHome className='h-full w-12' />
+          <AiOutlineHome className='h-full w-12' data-testid='icons' />
         </Link>
         <Link href='/kanban'>
-          <VscChecklist className='h-full w-12' />
+          <VscChecklist className='h-full w-12' data-testid='icons' />
         </Link>
       </div>
       <div className='lg:w-screen h-screen bg-stone-50 flex-col flex items-center'>
