467b91c0463caed2574cd6d6ccd876a07d594809
hkh0105 <hkh9601@naver.com>
2023-01-18 00:04:36 +0900

    rafactor : layout Refactor

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,22 +1,13 @@
-import Link from 'next/link';
 import { FC, PropsWithChildren } from 'react';
-import { VscChecklist } from 'react-icons/vsc';
-import { AiOutlineHome } from 'react-icons/ai';
+
+import NavigationBar from './navigationBar/navigationBar';
+import Section from './section/Section';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <div className='flex w-screnn h-20  bg-stone-50'>
-        <Link href='/'>
-          <AiOutlineHome className='h-full w-12' data-testid='icons' />
-        </Link>
-        <Link href='/kanban'>
-          <VscChecklist className='h-full w-12' data-testid='icons' />
-        </Link>
-      </div>
-      <div className='lg:w-screen h-screen bg-stone-50 flex-col flex items-center'>
-        {children}
-      </div>
+      <NavigationBar />
+      <Section>{children}</Section>
     </>
   );
 };
